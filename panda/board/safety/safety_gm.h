952ad945a620d7810836360310137722f4a47510
mohammed49x49 <80302679+mohammed49x49@users.noreply.github.com>
2021-04-13 16:34:34 -0500

    Update safety_gm.h

--- panda/board/safety/safety_gm.h
+++ panda/board/safety/safety_gm.h
@@ -14,7 +14,7 @@ typedef struct {
   uint32_t rolling_counter;
 } gm_dual_buffer;
 
-const int GM_MAX_STEER = 300;
+const int GM_MAX_STEER = 600;
 const int GM_MAX_RT_DELTA = 128;          // max delta torque allowed for real time checks
 const uint32_t GM_RT_INTERVAL = 250000;    // 250ms between real time checks
 const int GM_MAX_RATE_UP = 10;
@@ -262,7 +262,7 @@ static int gm_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
 }
 
 static int gm_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
-
+  return -1;
   int bus_fwd = -1;
   if (bus_num == 0) {
     if (gm_ffc_detected) {
